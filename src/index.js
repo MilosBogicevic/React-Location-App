b21db308bac0ef9d9bff03c7c753b5f409dfcbf9
MilosBogicevic <milbog.development@gmail.com>
2020-07-06 22:07:06 +0200

    Geolocation API added.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>React location app</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div></div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
