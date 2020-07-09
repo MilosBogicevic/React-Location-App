8ee110ad21e287023b9f628c5944388768a15bce
MilosBogicevic <milbog.development@gmail.com>
2020-07-09 18:51:44 +0200

    Initialize State in the Constructor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize State in the Constructor
+    this.state = {
+      lat: 30,
+      lon: 40,
+    };
+  }
+
   render() {
+    // Geolocation API - Ask users to provide their location to web application
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
     );
-    return <div></div>;
+    return (
+      <div>
+        Latitude: {this.state.lat} Longitude: {this.state.lon}
+      </div>
+    );
   }
 }
 
